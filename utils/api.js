8cc6a06e002cbfb0176fbb229889a85d9f08de68
Norberto Mantohac <nmantohac82098@gmail.com>
2020-04-15 15:38:08 -0700

    added token.js on .gitignore

--- utils/api.js
+++ utils/api.js
@@ -1,11 +1,9 @@
 const axios = require("axios");
-var config = {
-  headers: {'Authorization': 'token 598c2de3b11f6959952eb2edab734c75c4851e82'}
-}
+const token = require("./token")
 
 const api = {
   getUser(username, cb) {
-    axios.get(`https://api.github.com/users/${username}`, config)
+    axios.get(`https://api.github.com/users/${username}`, token.config)
       .then(response => {
         console.log(response.data);
         console.log(response.data.avatar_url);
